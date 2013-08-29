58ec67fb0f9634ea54ff4a6c56cb196943413f01
akey- <akey-@users.noreply.github.com>
2013-08-30 00:07:50 +0530

    program to implement bubble sort

--- Array/Sorting/BubbleSort.cpp
+++ Array/Sorting/BubbleSort.cpp
@@ -11,7 +11,7 @@ void bubble_sort(int a[],int n)
 {
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0;j<n-i-1;j++)
         {
             if(a[j]>a[j+1])
             {
